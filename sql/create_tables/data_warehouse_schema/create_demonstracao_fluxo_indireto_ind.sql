b71723f9f392400ab161ca79827eea731f71f5e0
samuelv8 <78799492+samuelv8@users.noreply.github.com>
2021-12-10 18:17:36 -0300

    Adding fk to new table on other tables

--- sql/create_tables/data_warehouse_schema/create_demonstracao_fluxo_indireto_ind.sql
+++ sql/create_tables/data_warehouse_schema/create_demonstracao_fluxo_indireto_ind.sql
@@ -1,11 +1,12 @@
 create table if not exists {schema}.demonstracao_fluxo_indireto_ind
 (
-	cnpj_cia varchar(20),
-	denom_cia varchar(100),
+	cia_id int not null,
 	ds_conta varchar(100),
 	cd_conta varchar(18),
 	vl_conta float,
 	quarter varchar(10),
 	dt_ini_exerc varchar(10),
-	dt_fim_exerc varchar(10)
+	dt_fim_exerc varchar(10),
+	constraint fk_cia
+        foreign key (cia_id) references {schema}.companies (id)
 )
--- sql/create_tables/data_warehouse_schema/create_demonstracao_mutacao_ind.sql
+++ sql/create_tables/data_warehouse_schema/create_demonstracao_mutacao_ind.sql
@@ -1,12 +1,13 @@
 create table if not exists {schema}.demonstracao_mutacao_ind
 (
-	cnpj_cia varchar(20),
-	denom_cia varchar(100),
+	cia_id int not null,
 	ds_conta varchar(100),
 	cd_conta varchar(18),
 	vl_conta float,
 	quarter varchar(10),
 	dt_ini_exerc varchar(10),
 	dt_fim_exerc varchar(10),
-	COLUNA_DF CHAR(60)
+	COLUNA_DF CHAR(60),
+	constraint fk_cia
+        foreign key (cia_id) references {schema}.companies (id)
 )
